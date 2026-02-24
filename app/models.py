c06175f082beeb5c5d3d97e6c77ec6da3a1fcc97
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-02-24 15:49:07 +0600

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,6 +1,8 @@
 from pydantic import BaseModel
+from typing import List, Optional
 
 class ModerationRequest(BaseModel):
     description: str
-    image: str
-    id: str 
+    image: Optional[str] = None
+    media: Optional[List[str]] = None
+    id: str
