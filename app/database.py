145b494fe5e88ab1eaacf505f2a0e9343a7765ab
Istiaque Ahmed <141866100+IstiaqueAhmd@users.noreply.github.com>
2026-02-24 15:06:38 +0600

    Update database.py

--- app/database.py
+++ app/database.py
@@ -1,6 +1,9 @@
 import os
 from motor.motor_asyncio import AsyncIOMotorClient
 from bson import ObjectId
+from dotenv import load_dotenv
+
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 client = AsyncIOMotorClient(DATABASE_URL)
